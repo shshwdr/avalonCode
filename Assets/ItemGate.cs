8294eab9384e38d30845b608c82fb493666ef86c
shshwdr <cs.vivian04@gmail.com>
2022-11-23 23:50:37 -0800

    add gate anim

--- Assets/ItemGate.cs
+++ Assets/ItemGate.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,16 +9,29 @@ public class ItemGate : MonoBehaviour,IStatable
     public Sprite openGateSprite;
     public SpriteRenderer renderer;
     BoxCollider collider;
+    public GameObject leftWing;
+    public GameObject rightWing;
+    public float openTime = 2;
+    public float openDegree = 45;
     public void stateChange()
     {
         if(GetComponent<StateItem>().state == 1)
         {
-            Destroy( GetComponentInChildren<Talkable>().gameObject);
-            renderer.sprite = openGateSprite;
-            collider.enabled = false;
-            ItemInventoryManager.Instance.removeTokenableItem("key");
-            QuestManager.Instance.finishQuest("openGate");
+            action();
         }
+    }
+
+    public void action()
+    {
+
+        Destroy(GetComponentInChildren<Talkable>().gameObject);
+        //renderer.sprite = openGateSprite;
+        //collider.enabled = false;
+        leftWing.transform.DORotate(new Vector3(0, openDegree, 0), openTime);
+        rightWing.transform.DORotate(new Vector3(0, -openDegree, 0), openTime);
+        ItemInventoryManager.Instance.removeTokenableItem("key");
+        QuestManager.Instance.finishQuest("openGate");
+        this.enabled = false;
     }
 
     // Start is called before the first frame update
@@ -29,6 +43,9 @@ public class ItemGate : MonoBehaviour,IStatable
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            action();
+        }
     }
 }
