1b3b31e79ddcb6f4ea7e2a9a35b7507ac58943cb
shshwdr <cs.vivian04@gmail.com>
2022-11-29 22:32:28 -0800

    some polish

--- Assets/BasicPlayerController.cs
+++ Assets/BasicPlayerController.cs
@@ -1,15 +1,23 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class BasicPlayerController : MonoBehaviour
 {
     public float speed;
+    public Sprite left;
+    public Sprite right;
+    public Sprite up;
+    public Sprite down;
+    public SpriteRenderer renderer;
     CharacterController controller;
     Rigidbody rb;
+    Animator anim;
     void Start()
     {
         controller = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,6 +32,40 @@ public class BasicPlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward*z;
         rb.AddForce(move * speed*Time.deltaTime);
+        var vel = new Vector3(x,0,z);
+        if (Mathf.Abs(vel.x)> Mathf.Abs(vel.z))
+        {
+            if (vel.x > 0.01f)
+            {
+                renderer.sprite = right;
+            }else if (vel.x < -0.01f)
+            {
+                renderer.sprite = left;
+            }
+        }
+        else
+        {
+            if (vel.z > 0.01f)
+            {
+                renderer.sprite = up;
+            }
+            else if (vel.z < -0.01f)
+            {
+                renderer.sprite = down;
+            }
+
+        }
+
+        if (vel.magnitude > 0.01f)
+        {
+            // renderer.transform.DORotate(new Vector3()
+            anim.SetBool("walking",true);
+        }
+        else
+        {
+
+            anim.SetBool("walking", false);
+        }
 
         //controller.Move(speed * Time.deltaTime * move);
     }
